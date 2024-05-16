16502dc8aadaab5745151a7e04c7ee94be8d7409
Kohki <124332011+Cookie-Knight@users.noreply.github.com>
2024-05-16 17:22:15 +0000

    bug fixes

---
+++ app/main/page.tsx
@@ -0,0 +1,156 @@
+export default function main() {
+    return (
+      <main >
+        <div className= "p-5">
+          <div className="flex justify-center items-center stats shadow">
+            <div className="stat">
+              <div className="stat-figure text-primary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-8 h-8 stroke-current"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M4.318 6.318a4.5 4.5 0 000 6.364L12 20.364l7.682-7.682a4.5 4.5 0 00-6.364-6.364L12 7.636l-1.318-1.318a4.5 4.5 0 00-6.364 0z"
+                  ></path>
+                </svg>
+              </div>
+              <div className="stat-title">Name</div>
+              <div className="stat-value text-primary">Bob Yapper</div>
+
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-secondary">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="inline-block w-8 h-8 stroke-current">
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 10V3L4 14h7v7l9-11h-7z"
+                  ></path>
+                </svg>
+              </div>
+              <div className="stat-title">Student ID</div>
+              <div className="stat-value text-secondary">10293011</div>
+            </div>
+
+            <div className="stat">
+              <div className="stat-figure text-secondary static">
+                <div className="avatar online">
+                  <div className="w-16 rounded-full">
+                    <img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+                  </div>
+                </div>
+              </div>
+              <div className="stat-title">CGPA</div>
+              <div className="stat-value">4.0</div>
+            </div>
+          </div>
+
+          <div className="flex flex-col w-full lg:flex-row p-10 ">
+    <div className="grid flex-grow h-auto card bg-base-300 rounded-box place-items-center static">
+    
+  <div className="overflow-x-auto">
+  <table className="table">
+    {/* head */}
+    <thead>
+      <tr>
+        <th></th>
+        <th>Name</th>
+        <th>Job</th>
+        <th>Favorite Color</th>
+      </tr>
+    </thead>
+    <tbody>
+      {/* row 1 */}
+      <tr>
+        <th>1</th>
+        <td>Cy Ganderton</td>
+        <td>Quality Control Specialist</td>
+        <td>Blue</td>
+      </tr>
+      {/* row 2 */}
+      <tr>
+        <th>2</th>
+        <td>Hart Hagerty</td>
+        <td>Desktop Support Technician</td>
+        <td>Purple</td>
+      </tr>
+      {/* row 3 */}
+      <tr>
+        <th>3</th>
+        <td>Brice Swyre</td>
+        <td>Tax Accountant</td>
+        <td>Red</td>
+      </tr>
+    </tbody>
+  </table>
+</div>
+    </div> 
+  <div className="divider lg:divider-horizontal"></div> 
+  <div className="grid flex-grow h-auto card bg-base-300 rounded-box place-items-center static">
+
+  <ul className="timeline">
+  <li>
+    <div className="timeline-start timeline-box">Semester 1</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <hr className="bg-primary"/>
+  </li>
+  <li>
+  <hr className="bg-primary"/>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-end timeline-box">Semester 2</div>
+    <hr className="bg-primary"/>
+  </li>
+  <li>
+    <hr className="bg-primary"/>
+    <div className="timeline-start timeline-box">Semester 3</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5 text-primary"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <hr/>
+  </li>
+  <li>
+    <hr/>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+    <div className="timeline-end timeline-box">Semester 4</div>
+    <hr/>
+  </li>
+  <li>
+    <hr/>
+    <div className="timeline-start timeline-box">Semester 5</div>
+    <div className="timeline-middle">
+      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20" fill="currentColor" className="w-5 h-5"><path fillRule="evenodd" d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z" clipRule="evenodd" /></svg>
+    </div>
+  </li>
+</ul>
+
+  </div>
+</div>
+
+<div className="mockup-browser border bg-base-300">
+  <div className="mockup-browser-toolbar">
+    <div className="input"> Education News!</div>
+  </div>
+  <div className="flex justify-center px-4 py-16 bg-base-200">more to add...</div>
+</div>
+
+        </div>
+      </main>
+    )
+}
