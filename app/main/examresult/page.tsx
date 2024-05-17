217cb3eb6cb8049159d4d5a0f4e0c516e74eff88
Kohki <124332011+Cookie-Knight@users.noreply.github.com>
2024-05-17 10:20:48 +0000

    added nested pages

---
+++ app/main/examresult/page.tsx
@@ -0,0 +1,12 @@
+export default function ExamResult() {
+    return (
+        <main>
+        <div className="mockup-browser border bg-base-300">
+  <div className="mockup-browser-toolbar">
+    <div className="input"> </div>
+  </div>
+  <div className="flex justify-center px-4 py-16 bg-base-200">more to add...</div>
+</div>
+</main>
+    )
+}
