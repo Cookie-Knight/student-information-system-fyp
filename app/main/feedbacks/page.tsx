e8026e91d6c088964a017894d22630a63dd3d7d9
Kohki <124332011+Cookie-Knight@users.noreply.github.com>
2024-06-16 06:35:29 +0000

    update on pages

--- app/main/feedbacks/page.tsx
+++ app/main/feedbacks/page.tsx
@@ -1,7 +1,40 @@
 export default function Feedbacks() {
     return (
         <main>
-<p></p>
-</main>
+            <div className="flex flex-col w-full">
+
+                
+                <div className="divider txt-slaete-400">Feedback Form</div>
+  
+                <div className="grid h-95 card bg-base-300 p-4 ml-4 mr-4 mb-4 mt-4 rounded-box">
+                    <div className="overflow-x-auto">
+                       
+                    <label className="input input-bordered flex items-center gap-2 mb-4">
+                     Name
+                    <input type="text" className="grow" placeholder="Student Name" />
+                        </label>
+                    <label className="input input-bordered flex items-center gap-2 mb-4">
+                    Email
+                    <input type="text" className="grow" placeholder="campushere@site.com" />
+                        </label>
+
+                        <label className="form-control w-full max-w-xs">
+                            
+                            <div className="label">
+                                <span className="label-text">Feedback Title</span>
+                            </div>
+
+                            <input type="text" placeholder="Type here" className="input input-bordered text-base w-full max-w-xs flex grow mb-4" />
+                        </label>
+
+                        <textarea className="textarea textarea-info w-full h-40 mb-4" placeholder="What is your feedbacks?"></textarea>
+                    </div>
+
+                    <button className="btn btn-xs sm:btn-sm md:btn-md lg:btn-lg">Submit Feedback</button>
+
+                </div>
+            </div>
+
+  </main>
     )
-}
+  }
--- app/main/perks/page.tsx
+++ app/main/perks/page.tsx
@@ -1,12 +1,24 @@
 export default function Perks() {
-    return (
-        <main>
-        <div className="mockup-browser border bg-base-300">
-  <div className="mockup-browser-toolbar">
-    <div className="input"> </div>
-  </div>
-  <div className="flex justify-center px-4 py-16 bg-base-200">more to add...</div>
+  return (
+      <main>
+<div className="flex flex-col w-full">
+<div className="grid h-20 card bg-base-300 rounded-box place-items-center p-4 ml-4 mr-4 mb-4 mt-4">CampuShere Perks</div> 
+<div className="divider"></div> 
+
+<div className="grid h-40 card bg-base-300 rounded-box place-items-center p-4 ml-4 mr-4 mb-2 mt-2">content</div>
+
+<div className="grid h-40 card bg-base-300 rounded-box place-items-center p-4 ml-4 mr-4 mb-2 mt-2">content</div>
+
+<div className="grid h-40 card bg-base-300 rounded-box place-items-center p-4 ml-4 mr-4 mb-2 mt-2">content</div>
+
+<div className="join justify-center mb-2">
+              <button className="join-item btn">«</button>
+              <button className="join-item btn">Page 1</button>
+              <button className="join-item btn">»</button>
 </div>
+
+</div>
+
 </main>
-    )
+  )
 }
