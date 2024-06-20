12574a7789ade8832c8932726333481a28c41261
Kohki <124332011+Cookie-Knight@users.noreply.github.com>
2024-06-20 17:17:58 +0000

    adjustment

--- app/main/programme/page.tsx
+++ app/main/programme/page.tsx
@@ -1,43 +1,43 @@
 export default function Programme() {
-    return (
-      <main>
-        <div className="flex flex-col w-full">
-        <div className="grid h-20 card bg-base-300 p-4 ml-4 mr-4 mb-4 mt-4 rounded-box font-bold text-2xl place-content-evenly">Programme History</div> 
+  return (
+    <main>
+      <div className="flex flex-col w-full">
+      <div className="grid h-20 card bg-base-300 p-4 ml-4 mr-4 mb-4 mt-4 rounded-box font-bold text-2xl place-content-evenly">Programme History</div> 
 
-          <div className="grid h-95 card bg-base-300 p-4 ml-4 mr-4 mb-4 mt-4 rounded-box">
+        <div className="grid h-95 card bg-base-300 p-4 ml-4 mr-4 mb-4 mt-4 rounded-box">
 
-          <div className="overflow-x-auto">
-  <table className="table">
-    {/* head */}
-    <thead>
-      <tr>
-        <th>Subject Code</th>
-        <th>Subject Name</th>
-      </tr>
-    </thead>
-    <tbody>
-      {/* row 1 */}
-      <tr>
-        <td>Diploma in Information Technology</td>
-        <td>3</td>
+        <div className="overflow-x-auto">
+<table className="table">
+  {/* head */}
+  <thead>
+    <tr>
+      <th>Subject Code</th>
+      <th>Subject Name</th>
+    </tr>
+  </thead>
+  <tbody>
+    {/* row 1 */}
+    <tr>
+      <td>Diploma in Information Technology</td>
+      <td>3</td>
 
-      </tr>
-      {/* row 2 */}
-      <tr className="hover">
-        <td>Diploma in Information Technology</td>
-        <td>2</td>
-      </tr>
-      {/* row 3 */}
-      <tr>
-        <td>Diploma in Information Technology</td>
-        <td>1</td>
+    </tr>
+    {/* row 2 */}
+    <tr className="hover">
+      <td>Diploma in Information Technology</td>
+      <td>2</td>
+    </tr>
+    {/* row 3 */}
+    <tr>
+      <td>Diploma in Information Technology</td>
+      <td>1</td>
 
-      </tr>
-    </tbody>
-  </table>
+    </tr>
+  </tbody>
+</table>
 </div>
 </div>
-        </div>
-      </main>
-    );
+      </div>
+    </main>
+  );
 }
