897777680e58b6b3a2bbaca1fa11d26b8a456d6a
JibbyKantoi <149048895+JibbyKantoi@users.noreply.github.com>
2024-06-24 03:30:04 +0000

    progress bar issue fix

--- app/main/page.tsx
+++ app/main/page.tsx
@@ -76,36 +76,43 @@ const Main: React.FC = () => {
     checkAuth();
   }, []); // Empty dependency array ensures useEffect runs only once
 
-  const renderSemester = (semesterNumber: number) => {
-    const isCompleted = semesterNumber <= studentData.completedSemesters;
-    return (
-      <li key={semesterNumber}>
-        {semesterNumber !== 1 && (
-          <hr className={isCompleted ? "bg-primary progress-bar-completed" : "bg-gray progress-bar"} />
-        )}
-        <div className={`timeline-box ${semesterNumber % 2 === 0 ? 'timeline-end' : 'timeline-start'}`}>
-          Semester {semesterNumber}
-        </div>
-        <div className="timeline-middle">
-          <svg
-            xmlns="http://www.w3.org/2000/svg"
-            viewBox="0 0 20 20"
-            fill="currentColor"
-            className={`w-5 h-5 ${isCompleted ? 'text-primary' : 'text-gray'}`}
-          >
-            <path
-              fillRule="evenodd"
-              d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-              clipRule="evenodd"
-            />
-          </svg>
-        </div>
-        {semesterNumber === studentData.completedSemesters && (
-          <hr className="bg-primary progress-bar-completed" />
-        )}
-      </li>
-    );
-  };
+ const renderSemester = (semesterNumber: number) => {
+  const isCompleted = semesterNumber <= studentData.completedSemesters;
+  return (
+    <li key={semesterNumber}>
+      {semesterNumber !== 1 && (
+        <hr
+          className={isCompleted ? "bg-primary progress-bar-completed" : "bg-gray progress-bar"}
+          style={{ margin: "0 auto", width: "200%", marginLeft: "-100%" }}
+        />
+      )}
+      <div className={`timeline-box ${semesterNumber % 2 === 0 ? 'timeline-end' : 'timeline-start'}`}>
+        Semester {semesterNumber}
+      </div>
+      <div className="timeline-middle">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          viewBox="0 0 20 20"
+          fill="currentColor"
+          className={`w-5 h-5 ${isCompleted ? 'text-primary' : 'text-gray'}`}
+        >
+          <path
+            fillRule="evenodd"
+            d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+            clipRule="evenodd"
+          />
+        </svg>
+      </div>
+      {semesterNumber === studentData.completedSemesters && (
+        <hr
+          className="bg-primary progress-bar-completed"
+          style={{ margin: "0 auto", width: "0%", marginLeft: "-110%" }}
+        />
+      )}
+    </li>
+  );
+};
+
 
   return (
     <main>
