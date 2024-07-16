7ebff1f6671df112346086d7621f25ffa2da8c00
Kohki Koh <takahashi.koh4@gmail.com>
2024-07-16 13:48:08 +0000

    modified:   lib/firebase/firebaseConfig.ts

--- lib/firebase/firebaseConfig.ts
+++ lib/firebase/firebaseConfig.ts
@@ -6,14 +6,17 @@ import { getStorage } from 'firebase/storage';
 
 
 // Your Firebase configuration object
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
-  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
+  apiKey: "AIzaSyCpnWTN9gldCcctFplwJe8pHumM08Og6cA",
+  authDomain: "final-year-project-cef3f.firebaseapp.com",
+  databaseURL: "https://final-year-project-cef3f-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "final-year-project-cef3f",
+  storageBucket: "final-year-project-cef3f.appspot.com",
+  messagingSenderId: "244063019761",
+  appId: "1:244063019761:web:b71801c59b1f7cb0dc3f50",
+  measurementId: "G-HTL26K6Q6P"
 };
 
 // Initialize Firebase
