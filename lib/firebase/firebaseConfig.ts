362282ce819c92b6ca2080212ee3dbdb1ea1a209
Kohki Koh <takahashi.koh4@gmail.com>
2024-07-16 13:45:29 +0000

    modified:   app/components/Clock.tsx 	modified:   app/components/Header.tsx 	modified:   app/components/Loading.tsx 	modified:   app/components/Particles.tsx 	modified:   app/components/Time.tsx 	modified:   app/examresult/page.tsx 	modified:   app/page.tsx 	modified:   app/timetable/page.tsx 	modified:   lib/firebase/firebaseConfig.ts

--- lib/firebase/firebaseConfig.ts
+++ lib/firebase/firebaseConfig.ts
@@ -7,13 +7,13 @@ import { getStorage } from 'firebase/storage';
 
 // Your Firebase configuration object
 const firebaseConfig = {
-  apiKey: "AIzaSyCpnWTN9gldCcctFplwJe8pHumM08Og6cA",
-  authDomain: "final-year-project-cef3f.firebaseapp.com",
-  projectId: "final-year-project-cef3f",
-  storageBucket: "final-year-project-cef3f.appspot.com",
-  messagingSenderId: "244063019761",
-  appId: "1:244063019761:web:b71801c59b1f7cb0dc3f50",
-  measurementId: "G-HTL26K6Q6P"
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  measurementId: process.env.NEXT_PUBLIC_FIREBASE_MEASUREMENT_ID
 };
 
 // Initialize Firebase
